fc56599ba79b8394d4d0cb7d95ff41838fc1238b
IgnacioCantoni <ignaciomatiascantoni2001@gmail.com>
2026-06-11 11:26:07 -0300

    xd3

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable, inject, signal, computed } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable, tap, catchError, throwError } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.production';
 import { User } from '../models/prode.model';
 
 @Injectable({ providedIn: 'root' })
